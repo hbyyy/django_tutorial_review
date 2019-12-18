fbeb6e6f8970c3a06935c1db3c98427186877cb3
hbyyy <qjaduddl94@gmail.com>
2019-12-18 21:24:57 +0900

    장고 튜토리얼 part3, renderr() 이전까지, index.html 수정 후 views의 index 함수 수정 중

--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,24 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+from django.shortcuts import render
+
+from polls.models import Question
+
+
 def index(request):
-    return HttpResponse('Hello django world!')
+    questions = Question.objects.all()
+    context = {'questions': questions}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    return HttpResponse(f'detail {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'results {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'vote {question_id}')
